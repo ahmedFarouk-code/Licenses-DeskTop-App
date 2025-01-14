0e2bc954b07acd963b75a6fc748072fd219de070
ahmed farouk <faroukahmed804@gmail.com>
2025-01-15 00:48:04 +0200

    update files

--- LicensesBusinessLayer/clsPeople.cs
+++ LicensesBusinessLayer/clsPeople.cs
@@ -11,9 +11,110 @@ namespace LicensesBusinessLayer
     public class clsPeople
     {
 
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string ThirdName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Gendor { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int NationalityCountryID { get; set; }
+        public string ImagePath { get; set; }
+
+
+        private clsPeople(int ID, string FirstName, string SecondName, string ThirdName, string LastName, DateTime DateOfBirth, int Gendor, string Address, string Phone, string Email, int NationalityCountryID, string ImagePath)
+        {
+            this.ID = ID;
+            this.FirstName = FirstName;
+            this.SecondName = SecondName;
+            this.ThirdName = ThirdName;
+            this.LastName = LastName;
+            this.DateOfBirth = DateOfBirth;
+            this.Gendor = Gendor;
+            this.Address = Address;
+            this.Phone = Phone;
+            this.Email = Email;
+            this.NationalityCountryID = NationalityCountryID;
+            this.ImagePath = ImagePath;
+
+            Mode = enMode.Update;
+        }
+
+
+        public clsPeople()
+        {
+            this.ID = -1;
+            this.FirstName = "";
+            this.SecondName = "";
+            this.ThirdName = "";
+            this.LastName = "";
+            this.DateOfBirth = DateTime.Now;
+            this.Gendor = 0;
+            this.Address = "";
+            this.Phone = "";
+            this.Email = "";
+            this.NationalityCountryID = -1;
+            this.ImagePath = "";
+
+            Mode = enMode.AddNew;
+        }
+
+
+        private bool _AddNewPerson()
+        {
+            this.ID = clsPeopleData.AddNewPerson(this.FirstName, this.SecondName,
+                this.ThirdName,this.LastName, this.DateOfBirth,this.Gendor,this.Address,
+                this.Phone,this.Email,this.NationalityCountryID,this.ImagePath);
+
+            return (this.ID != -1);
+        }
+
+
+        private bool _UpdateContact()
+        {
+            return clsPeopleData.UpdatePerson(this.ID,this.FirstName, this.SecondName,
+                this.ThirdName, this.LastName, this.DateOfBirth, this.Gendor, this.Address,
+                this.Phone, this.Email, this.NationalityCountryID, this.ImagePath);
+        }
+
+
         public static DataTable GetAllPeople()
         {
             return clsPeopleData.GetAllPeople();
         }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    {
+                        if(_AddNewPerson())
+                        {
+                            Mode = enMode.Update;
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                case enMode.Update:
+
+                    return _UpdateContact();
+
+            }
+
+
+
+            return false;
+        }
+
     }
 }
--- clsPeopleData/clsPeopleData.cs
+++ clsPeopleData/clsPeopleData.cs
@@ -10,6 +10,123 @@ namespace LicensesDataAccess
 {
     public class clsPeopleData
     {
+        public static int AddNewPerson( string FirstName,  string SecondName,
+             string ThirdName ,  string LastName,  DateTime DateOfBirth,
+             int Gendor,  string Address,  string Phone,  string Email,
+             int NationalityCountryID,  string ImagePath)
+        {
+            int PersonID = -1;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString);
+            string query = @"INSERT INTO People (FirstName, SecondName, ThirdName,
+                              LastName, DateOfBirth, Gendor, Address, Phone, Email,
+                              NationalityCountryID, ImagePath) 
+                              VALUES(@FirstName, @SecondName, @ThirdName,
+                              @LastName, @DateOfBirth, @Gendor, @Address, @Phone, @Email,
+                              @NationalityCountryID, @ImagePath);
+                              SELECT SCOPE_IDENTITY();";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@FirstName",FirstName);
+            command.Parameters.AddWithValue("@SecondName", SecondName);
+            command.Parameters.AddWithValue("@ThirdName", ThirdName);
+            command.Parameters.AddWithValue("@LastName", LastName);
+            command.Parameters.AddWithValue("@DateOfBirth", DateOfBirth);
+            command.Parameters.AddWithValue("@Gendor", Gendor);
+            command.Parameters.AddWithValue("@Address", Address);
+            command.Parameters.AddWithValue("@Phone", Phone);
+            command.Parameters.AddWithValue("@Email", Email);
+            command.Parameters.AddWithValue("@NationalityCountryID", NationalityCountryID);
+
+            if (ImagePath != "" && ImagePath != null)
+                command.Parameters.AddWithValue("@ImagePath", ImagePath);
+            else
+                command.Parameters.AddWithValue("@ImagePath", System.DBNull.Value);
+
+
+            try
+            {
+                connection.Open();
+                object result = command.ExecuteScalar();
+                if(result !=null && int.TryParse(result.ToString(), out int insertedID))
+                {
+                    PersonID = insertedID;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return PersonID;
+        }
+
+
+        public static bool UpdatePerson(int ID,string FirstName, string SecondName,
+             string ThirdName, string LastName, DateTime DateOfBirth,
+             int Gendor, string Address, string Phone, string Email,
+             int NationalityCountryID, string ImagePath)
+        {
+            int RowsEffected = -1;
+            SqlConnection connection = new SqlConnection(clsDataAccessSetting.ConnectionString);
+            string query = @"UPDATE People
+                               SET FirstName = @FirstName,
+                                   SecondName = @SecondName,
+                                   ThirdName = @ThirdName,
+                                   LastName = @LastName,
+                                   DateOfBirth = @DateOfBirth,
+                                   Gendor = @Gendor,
+                                   Address = @Address,
+                                   Phone = @Phone,
+                                   Email = @Email,
+                                   NationalityCountryID = @NationalityCountryID,
+                                   ImagePath = @ImagePath
+                                   where ContactID = @ContactID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@ContactID", ID);
+            command.Parameters.AddWithValue("@FirstName", FirstName);
+            command.Parameters.AddWithValue("@SecondName", SecondName);
+            command.Parameters.AddWithValue("@ThirdName", ThirdName);
+            command.Parameters.AddWithValue("@LastName", LastName);
+            command.Parameters.AddWithValue("@DateOfBirth", DateOfBirth);
+            command.Parameters.AddWithValue("@Gendor", Gendor);
+            command.Parameters.AddWithValue("@Address", Address);
+            command.Parameters.AddWithValue("@Phone", Phone);
+            command.Parameters.AddWithValue("@Email", Email);
+            command.Parameters.AddWithValue("@NationalityCountryID", NationalityCountryID);
+           
+
+
+            if (ImagePath != "" && ImagePath != null)
+                command.Parameters.AddWithValue("@ImagePath", ImagePath);
+            else
+                command.Parameters.AddWithValue("@ImagePath", System.DBNull.Value);
+
+            try
+            {
+                connection.Open();
+                RowsEffected = command.ExecuteNonQuery();
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return (RowsEffected > 0);
+
+        }
+
+
         public static DataTable GetAllPeople()
         {
             DataTable dt = new DataTable();
